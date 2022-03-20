32ee5c4f96bd8b07c1dfa00379c327d0d5526eff
Eslim Daga <esliim9811@gmail.com>
2022-03-20 18:59:22 -0500

    Se agrego el componente toggle para poder cambiar el tema a oscuro y viceversa

---
+++ src/components/common/ThemeToggle.js
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { SunIcon, MoonIcon } from "@heroicons/react/solid";
+import { ThemeContext } from "../../contexts/ThemeContext";
+
+const Toggle = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  return (
+    <div className="transition duration-500 ease-in-out rounded-full p-2">
+      {theme === "dark" ? (
+        <SunIcon
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          className="text-gray-500 dark:text-gray-400 w-5 h-5 cursor-pointer"
+        />
+      ) : (
+        <MoonIcon
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          className="text-gray-500 dark:text-gray-400 w-5 h-5 cursor-pointer"
+        />
+      )}
+    </div>
+  );
+};
+
+export default Toggle;
