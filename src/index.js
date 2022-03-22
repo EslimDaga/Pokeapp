02b6d15582732553a9f58718f167cbdc6c9c5643
Eslim Daga <esliim9811@gmail.com>
2022-03-22 11:24:39 -0500

    Se agrego el browser router en el app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import "./assets/styles/main.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
