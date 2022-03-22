4055546b2652fbdb46485f736203e9ced58cd47d
Eslim Daga <esliim9811@gmail.com>
2022-03-22 11:42:35 -0500

    Importe el Link y los remplaze con las etiquetas <a/>

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,23 +1,24 @@
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Link } from "react-router-dom";
 import Toggle from "./ThemeToggle";
 import Logo from "../../assets/images/pokeapi.png";
 
 export default function Header() {
   return (
-    <Popover className="relative bg-white">
+    <Popover className="relative bg-white dark:bg-gray-900">
       <div className="mx-auto px-4 sm:px-4">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-4 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="/">
+            <Link to="/">
               <span className="sr-only">Logo</span>
               <img
                 className="h-8 w-auto sm:h-10"
                 src={Logo}
                 alt="logo-pokeapi"
               />
-            </a>
+            </Link>
           </div>
           <div className="-mr-2 -my-2 md:hidden">
             <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -26,12 +27,12 @@ export default function Header() {
             </Popover.Button>
           </div>
           <Popover.Group as="nav" className="hidden md:flex space-x-10">
-            <a
-              href="/"
+            <Link
+              to="/wath-pokemon-am-i"
               className="font-fredoka font-medium text-gray-500 hover:text-gray-900"
             >
               ¿Que pokemon soy?
-            </a>
+            </Link>
           </Popover.Group>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
             <Toggle />
@@ -62,11 +63,7 @@ export default function Header() {
             <div className="pt-5 pb-6 px-5">
               <div className="flex items-center justify-between">
                 <div>
-                  <img
-                    className="h-8 w-auto"
-                    src={Logo}
-                    alt="logo-pokeapi"
-                  />
+                  <img className="h-8 w-auto" src={Logo} alt="logo-pokeapi" />
                 </div>
                 <div className="-mr-2">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
