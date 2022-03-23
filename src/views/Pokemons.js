f0cdc5605b5c229ec2ab97dfe81718ee60e89d82
Eslim Daga Castro <esliim9811@gmail.com>
2022-03-23 18:54:10 -0500

    Ya se pueden leer los pokemones usando UseEffect, ojo queda pendiente entender bien Promises.all

--- src/views/Pokemons.js
+++ src/views/Pokemons.js
@@ -1,7 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Pokemons = () => {
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=100")
+    .then(response => {
+      const promises = response.data.results.map(item => axios.get(item.url));
+      Promise.all(promises).then((result) => {
+        setPokemons(result);
+      })
+    })
+  },[])
+
   return (
-    <div>Pokemons</div>
-  )
+    <div>
+      <ul>
+        {pokemons.map(({data}) => (
+          <>
+            <img src={data.sprites.back_default} alt={data.name} />
+            <li>{data.name}</li>
+          </>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Pokemons;
