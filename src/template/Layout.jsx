89d1d2979ac410eea0420fc158a3b078c21d8063
Eslim Daga Castro <esliim9811@gmail.com>
2022-03-24 18:57:38 -0500

    Añadi el envolvente pokeapp con styled components

--- src/template/Layout.jsx
+++ src/template/Layout.jsx
@@ -1,11 +1,18 @@
 import Header from "../components/common/Header";
+import styled from "styled-components";
+
+const PokeApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <PokeApp>
       <Header />
       { children }
-    </>
+    </PokeApp>
   )
 }
 
