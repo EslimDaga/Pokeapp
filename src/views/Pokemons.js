0a88f82fc88a879f1e04051d1be41374e54a6bc5
Eslim Daga <esliim9811@gmail.com>
2022-03-23 22:48:24 -0500

    Hay una pequeña mejora en la parte visual de los cards, recuerda Eslim si quieres centrar puedes ayudarte con un margin left y un margin right

--- src/views/Pokemons.js
+++ src/views/Pokemons.js
@@ -16,16 +16,30 @@ const Pokemons = () => {
   },[])
 
   return (
-    <div>
-      <ul>
-        {pokemons.map(({data}) => (
-          <>
-            <img src={data.sprites.back_default} alt={data.name} />
-            <li>{data.name}</li>
-          </>
+    <section className="px-4 py-4 bg-gray-100 dark:bg-gray-800">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {pokemons.map(({ data }) => (
+          <div
+            key={data.id}
+            className="max-w-sm rounded-xl bg-white dark:bg-gray-900 overflow-hidden shadow-lg"
+          >
+            <img
+              className="w-auto mx-auto mt-4 rounded-lg bg-gray-200 dark:bg-gray-700"
+              src={data.sprites.front_default}
+              alt="Sunset in the mountains"
+            />
+            <div className="px-6 py-4">
+              <div className="text-gray-700 dark:text-white capitalize font-fredoka font-semibold text-center mb-2">
+                {data.name}
+              </div>
+              <p className="text-gray-700 dark:text-white font-fredoka font-medium text-center">
+                Experiencia : {data.base_experience}
+              </p>
+            </div>
+          </div>
         ))}
-      </ul>
-    </div>
+      </div>
+    </section>
   );
 }
 
