6b7d3da6b36e0a8eca6dd064af8aaecca48c52d9
Eslim Daga <esliim9811@gmail.com>
2022-03-25 23:22:15 -0500

    Agregue la paginación para los pokemones

--- src/views/Pokemons.js
+++ src/views/Pokemons.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import { SearchCircleIcon, XCircleIcon } from "@heroicons/react/solid";
+import { SearchCircleIcon, XCircleIcon, ArrowCircleLeftIcon, ArrowCircleRightIcon } from "@heroicons/react/solid";
 
 const ClearInput = styled.div`
   position: absolute;
@@ -44,29 +44,31 @@ const Pokemons = () => {
     setPokemons(listPokemons);
   }
 
-  console.log("List Pokemons",listPokemons);
-  console.log("Pokemons",pokemons);
-  console.log("Filtering",filtering);
-
   return (
     <section className="flex-1 overflow-y-auto px-4 py-4 bg-gray-100 dark:bg-gray-800">
-      <div className="relative mb-4">
-        <input
-          type="text"
-          className="w-full sm:w-full md:w-64 lg-64 h-14 bg-white dark:bg-gray-900 placeholder:text-gray-600 text-gray-700 dark:text-white pr-8 pl-11 rounded-lg z-0 focus:shadow focus:outline-yellow-400 dark:focus:shadow dark:focus:outline-none font-fredoka font-normal"
-          placeholder="Buscar Pokemon"
-          value={filtering}
-          onChange={handleChange}
-        />
-        {filtering.length > 0 ? (
-          <ClearInput onClick={clearFilter}>
-            <XCircleIcon className="w-6 h-6 text-red-500" />
-          </ClearInput>
-        ) : (
-          <div className="absolute top-4 left-3">
-            <SearchCircleIcon className="w-6 h-6 text-gray-600" />
-          </div>
-        )}
+      <div className="relative sm:flex md:flex lg:flex justify-between items-center mb-4">
+        <div className="relative">
+          <input
+            type="text"
+            className="w-full sm:w-64 md:w-64 lg-64 h-14 bg-white dark:bg-gray-900 placeholder:text-gray-600 text-gray-700 dark:text-white pr-8 pl-11 rounded-lg z-0 focus:shadow focus:outline-yellow-400 dark:focus:shadow dark:focus:outline-none font-fredoka font-normal"
+            placeholder="Buscar Pokemon"
+            value={filtering}
+            onChange={handleChange}
+          />
+          {filtering.length > 0 ? (
+            <ClearInput onClick={clearFilter}>
+              <XCircleIcon className="w-6 h-6 text-red-500" />
+            </ClearInput>
+          ) : (
+            <div className="absolute top-4 left-3">
+              <SearchCircleIcon className="w-6 h-6 text-gray-600" />
+            </div>
+          )}
+        </div>
+        <div className="flex justify-center pt-4 sm:pt-0 md:pt-0 lg:pt-0">
+          <button><ArrowCircleLeftIcon className="w-10 h-10 text-blue-800 dark:text-gray-600" /></button>
+          <button><ArrowCircleRightIcon className="w-10 h-10 text-blue-800 dark:text-gray-600" /></button>
+        </div>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         {pokemons.map(({ data }) => (
