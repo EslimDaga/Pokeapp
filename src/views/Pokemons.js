77a6fe65041a9bb7850017c7c73bf45d333ae478
Eslim Daga Castro <esliim9811@gmail.com>
2022-03-30 18:26:45 -0500

    Oculte la paginación cuando hacen una busqueda

--- src/views/Pokemons.js
+++ src/views/Pokemons.js
@@ -85,20 +85,26 @@ const Pokemons = () => {
           )}
         </div>
         <div className="flex justify-center pt-4 sm:pt-0 md:pt-0 lg:pt-0">
-          <button
-            onClick={() => previousPage(paging.previous)}
-            disabled={paging.previous ? false : true}
-            className={!paging.previous ? "cursor-not-allowed opacity-50" : ""}
-          >
-            <ArrowCircleLeftIcon className="w-10 h-10 text-blue-800 dark:text-gray-600" />
-          </button>
-          <button
-            onClick={() => nextPage(paging.next)}
-            disabled={paging.next ? false : true}
-            className={!paging.next ? "cursor-not-allowed opacity-50" : ""}
-          >
-            <ArrowCircleRightIcon className="w-10 h-10 text-blue-800 dark:text-gray-600" />
-          </button>
+          {filtering.length <= 0 && (
+            <>
+              <button
+                onClick={() => previousPage(paging.previous)}
+                disabled={paging.previous ? false : true}
+                className={
+                  !paging.previous ? "cursor-not-allowed opacity-50" : ""
+                }
+              >
+                <ArrowCircleLeftIcon className="w-10 h-10 text-blue-800 dark:text-gray-600" />
+              </button>
+              <button
+                onClick={() => nextPage(paging.next)}
+                disabled={paging.next ? false : true}
+                className={!paging.next ? "cursor-not-allowed opacity-50" : ""}
+              >
+                <ArrowCircleRightIcon className="w-10 h-10 text-blue-800 dark:text-gray-600" />
+              </button>
+            </>
+          )}
         </div>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
