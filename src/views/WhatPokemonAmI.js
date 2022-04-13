b9adcf58da99e7fcec8aa98a7a2df684a0dd98c9
Eslim Daga <esliim9811@gmail.com>
2022-04-12 22:14:01 -0500

    Ahora el proyecto esta por terminar

--- src/views/WhatPokemonAmI.js
+++ src/views/WhatPokemonAmI.js
@@ -1,7 +1,30 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Pokemon from "../components/Pokemons/Pokemon";
+
 const WhatPokemonAmI = () => {
+
+  const navigate = useNavigate();
+
+  const [randomPokemon, setRandomPokemon] = useState([]);
+
+  useEffect(() => {
+    const random_number = Math.floor(Math.random() * (898 - 1 + 1) + 1);
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/" + random_number)
+      .then((res) => {
+        setRandomPokemon([res.data]);
+      });
+  }, []);
+
+  console.log(randomPokemon);
+
   return (
     <section className="flex-1 overflow-y-auto grid place-items-center px-4 py-4 bg-gray-100 dark:bg-gray-800">
-      <div class="">Centrado</div>
+      {randomPokemon.map((data) => (
+        <Pokemon key={data.id} data={data} navigate={navigate} />
+      ))}
     </section>
   );
 };
